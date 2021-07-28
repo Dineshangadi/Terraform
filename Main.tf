e5ee91d617b1680ebfc5856653d2d62b67359767
Dineshangadi <86283106+Dineshangadi@users.noreply.github.com>
2021-07-28 18:48:36 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,9 +1,31 @@
 resource "aws_vpc" "vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.cidir
   instance_tenancy = "default"
-
+  enable_dns_hostnames = true
   tags = {
     Name = "Dinesh"
   }
 }
 
+#Subnet Public
+resource "aws_subnet" "pub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "var.public cidr"
+  availability_zone       = "ap-southeast-1"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pub"
+  }
+}
+
+#private sub
+resource "aws_subnet" "pri" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "var.private cidr"
+  availability_zone       = "ap-southeast-2"
+
+  tags = {
+    Name = "pub"
+  }
+}
